9b73e534bb244d07afb10b77da5dbf21c0433a82
Philippe <phildoreau45@gmail.com>
2021-02-28 12:07:42 +0100

    update SEO missing import

--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -2,6 +2,7 @@ import { graphql } from 'gatsby';
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import SEO from '../components/SEO';
 
 export default function Slicemaster({ data: { person } }) {
   return (
